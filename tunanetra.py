5d9d228a1233cc2cfc44922fe2f1c90eb15d74c5
usupsuparma <usup.suparma7@gmail.com>
2018-07-27 22:55:25 +0700

    perubahan program

--- tunanetra.py
+++ tunanetra.py
@@ -1,69 +1,58 @@
 import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)  # set penomeran board ke mode broadcom
-GPIO.setwarnings(False)
 
-# Set mode pin sebagai input untuk trigger, dan output untuk echo
-GPIO_TRIGGER = 23 #sesuaikan pin trigger
-GPIO_ECHO = 24 #sesuaikan pin echo
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT)
-GPIO.setup(GPIO_ECHO,GPIO.IN)
 
-# Set trigger ke False (Low) untuk awal
-GPIO.output(GPIO_TRIGGER, GPIO.HIGH)
-
-#Fungsi untuk menapatkan jarak
-def get_range():
-    # Kirim 10us sinyal high ke trigger
-    GPIO.output(GPIO_TRIGGER, True)
+def measure():
+    GPIO.output(trig, True)
     time.sleep(0.00001)
-
-    #Stop trigger
-    GPIO.output(GPIO_TRIGGER, False)
-    timeout_counter = int(time.time())
+    GPIO.output(trig, False)
     start = time.time()
 
-    #dapatkan waktu start
-    while GPIO.input(GPIO_ECHO)==0 and (int(time.time()) - timeout_counter) < 3:
+    while GPIO.input(echo == 0):
         start = time.time()
 
-    timeout_counter = int(time.time())
-    stop = time.time()
-    #dapatkan waktu stop
-    while GPIO.input(GPIO_ECHO)==1 and (int(time.time()) - timeout_counter) < 3:
+    while GPIO.input(echo == 1):
         stop = time.time()
-
-    #Hitung waktu tempuh bolak-balik
-    elapsed = stop-start
-
-    #Hitung jarak, waktu tempuh dikalikan dengan kecepata suara (dalam centi meter)
-    distance = elapsed * 34320
-
-    #Jaraknya masih dalam hitungan bolak-balik, bagi dua untuk tahu jarak ke halangan
-    distance = distance / 2
-
-    #selesai
+    elepsed = stop - start
+    distance = (elepsed * 34300)/2
+    return distance
+def measure_average():
+    #this
+    distance1 = measure()
+    time.sleep(0.1)
+    distance2 = measure()
+    time.sleep(0.1)
+    distance3 = measure()
+    time.sleep(0.1)
+    distance = distance1 + distance2 + distance3
+    distance = distance/3
     return distance
 
 
+# main program
 
-if __name__ == '__main__':
-    try:
-        while True:
-            #jarak
-            dist = get_range()
-            print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
-            if dist <10:
-                print("bahaya")
-            elif dist >=10 and dist < 20:
-                print("hati2")
-            elif dist >= 20 :
-                print("aman")
-            else:
-                print("error")
 
-        # Reset by pressing CTRL + C
-    except KeyboardInterrupt:
-        print(" Measurement stopped by User ")
-        GPIO.cleanup()
+# GPIO
+GPIO.setmode(GPIO.BCM)
+
+
+# GPIO yang digunakan
+trig = 23
+echo = 24
+
+print("ultrasonik measuring")
+
+# set pin output and input
+GPIO.setup(trig, GPIO.OUT) # triger
+GPIO.setup(echo, GPIO.IN) # echo
+
+GPIO.output(trig , False)
+
+try:
+    while True:
+        distance = measure_average()
+        print("jarak adalah: %.1f "% distance)
+        time.sleep(1)
+except KeyboardInterrupt:
+    # gunakan ctrl + c
+    GPIO.cleanup()
