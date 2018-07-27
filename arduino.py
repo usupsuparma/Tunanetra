ebacac743d154fc75966ce36cd2874e7f27b6e9c
usupsuparma <usup.suparma7@gmail.com>
2018-07-27 23:55:13 +0700

    add perubahan

--- arduino.py
+++ arduino.py
@@ -6,19 +6,16 @@ output = " "
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 
 
-#while True:
-#  print "----"
-#  while output != "":
-#    output = ser.readline()
-#    print output
-#  output = " "
+
 
 try:
     while True:
-        for i in range(0,256):
-            print(unichr(i))
-            ser.write(chr(i))
-            time.sleep(1)
+        print ("----")
+        while output != (" "):
+            output = ser.readline()
+            print("hasil: ",output)
+        output = " "
+
 except KeyboardInterrupt:
     # gunakan ctrl + c
     GPIO.cleanup()
