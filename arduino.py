fdc4b66723b86b9710067ec5c058b7fe0aa88c49
usupsuparma <usup.suparma7@gmail.com>
2018-07-28 00:03:37 +0700

    a

--- arduino.py
+++ arduino.py
@@ -2,7 +2,7 @@
 
 import serial, string, time
 
-output = " "
+output = (" ")
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 
 while True:
@@ -10,7 +10,7 @@ while True:
         print("----")
         while output != (""):
             output = ser.readline()
-            print output
+            print(output)
         output = (" ")
     except KeyboardInterrupt:
         print("keluar dari program")
