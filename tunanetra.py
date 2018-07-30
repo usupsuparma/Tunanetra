e0b3af0d4f468a747349f1cb89dd3be57864729e
usupsuparma <usup.suparma7@gmail.com>
2018-07-30 17:34:03 +0700

    perbaikan jarak

--- tunanetra.py
+++ tunanetra.py
@@ -1,58 +1,27 @@
 import time
 import RPi.GPIO as GPIO
+import serial, string, time
 
-
-def measure():
-    GPIO.output(trig, True)
-    time.sleep(0.00001)
-    GPIO.output(trig, False)
-    start = time.time()
-
-    while GPIO.input(echo == 0):
-        start = time.time()
-
-    while GPIO.input(echo == 1):
-        stop = time.time()
-    elepsed = stop - start
-    distance = (elepsed * 34300)/2
-    return distance
-def measure_average():
-    #this
-    distance1 = measure()
-    time.sleep(0.1)
-    distance2 = measure()
-    time.sleep(0.1)
-    distance3 = measure()
-    time.sleep(0.1)
-    distance = distance1 + distance2 + distance3
-    distance = distance/3
-    return distance
 
 
 # main program
 
+output = (" ")
+ser = serial.Serial('/dev/ttyACM0', 9600)
 
-# GPIO
-GPIO.setmode(GPIO.BCM)
-
-
-# GPIO yang digunakan
-trig = 23
-echo = 24
-
-print("ultrasonik measuring")
-
-# set pin output and input
-GPIO.setup(trig, GPIO.OUT) # triger
-GPIO.setup(echo, GPIO.IN) # echo
-
-GPIO.output(trig , False)
 
 try:
     while True:
-        distance = measure_average()
-        print("jarak adalah: %.1f "% distance)
-        time.sleep(1)
+        if output != " ":
+            output =str(int(ser.readline()))
+            if output <= 50:
+                print(output)
+                print("jarak bahaya")
+            elif output >= 50:
+                print("jarak aman")
+        else:
+            print("data jarak tidak ada")
+
 except KeyboardInterrupt:
     # gunakan ctrl + c
     GPIO.cleanup()
