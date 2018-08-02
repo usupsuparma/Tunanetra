f22c6c2c9a395accc74aba8043cc39f983c600d4
usupsuparma <usup.suparma7@gmail.com>
2018-08-02 11:08:22 +0700

    perubahan lagi pada while

--- jarak.py
+++ jarak.py
@@ -3,19 +3,16 @@
 import string, time
 import serial
 import time
-import RPi.GPIO as GPIO
 
 
 output = (" ")
 ser =serial.Serial('/dev/ttyACM0', 9600)
 
-try:
-    while True:
-        print("----")
-        while output != (""):
-            output =int(ser.readline())
-            print(output)
-            output = (" ")
-except KeyboardInterrupt:
-    print("keluar dari program euy")
-    # gunakan ctrl + c
+while True:
+    try:
+        output = int(ser.readline())
+        print("hasil terbaca: ",output)
+    except KeyboardInterrupt as e:
+        print("Exit Program")
+        print("============")
+        break
