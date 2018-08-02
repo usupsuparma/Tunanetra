e4d7a726c6c3ca1398a00743f09a1e74b74ce8a8
usupsuparma <usup.suparma7@gmail.com>
2018-08-02 13:19:15 +0700

    as

--- jarak.py
+++ jarak.py
@@ -7,5 +7,9 @@ import time
 
 #ser =serial.Serial('/dev/ttyACM0', 9600)
 ser = serial.Serial('/dev/ttyACM0', 9600)
-output = int(ser.readline())
-print("hasil terbaca: ",output)
+while True:
+    try:
+        result = int(ser.readline())
+        print("hasil: ",result)
+    except KeyboardInterrupt as e:
+        break
